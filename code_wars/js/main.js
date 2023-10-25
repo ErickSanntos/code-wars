52b022e436a4c3158d2d68e86efc1db60718e9ee
erickson santos <santoselr39@gmail.com>
2023-10-24 20:26:46 -0400

    new

--- code_wars/js/main.js
+++ code_wars/js/main.js
@@ -163,3 +163,13 @@ function past(h, m, s){
   // returns time in milliseconds
   // 1 hour = 3600 second 
   // 3600 * 1000 = milliseconds 
+
+
+  //Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+  
+function litres(time) {
+    let drinks = Math.floor(time * 0.5)
+    return drinks;
+  }
